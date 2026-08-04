715dd0c2ba480406f7825340bcf4e03d0e97f847
Bruno Celaries <bruno.celaries@gmail.com>
2026-08-05 00:55:00 +0200

    Add bpool creation

---
+++ tasks/task_07_create_bpool.py
@@ -0,0 +1,34 @@
+class CreateBootPool(Task):
+    name = "Create ZFS boot pool"
+
+    def __init__(self, boot_partition, root_mount="/mnt"):
+        self.boot_partition = boot_partition
+        self.root_mount = root_mount
+
+    def check(self):
+        return exists([
+            "zpool",
+            "list",
+            "bpool",
+        ])
+
+    def execute(self):
+        return run([
+            "zpool",
+            "create",
+            "-o", "ashift=12",
+            "-o", "autotrim=on",
+            "-o", "compatibility=grub2",
+            "-o", "cachefile=/etc/zfs/zpool.cache",
+            "-O", "devices=off",
+            "-O", "acltype=posixacl",
+            "-O", "xattr=sa",
+            "-O", "compression=lz4",
+            "-O", "normalization=formD",
+            "-O", "relatime=on",
+            "-O", "canmount=off",
+            "-O", "mountpoint=/boot",
+            "-R", self.root_mount,
+            "bpool",
+            self.boot_partition,
+        ])
